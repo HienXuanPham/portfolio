0a99e40330f7b5d657fb2d72f15b11cd554c5611
HienXuanPham <phamxuanhien02@gmail.com>
2023-05-14 17:46:54 -0400

    added animation

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,12 +1,16 @@
 import React from "react";
 import "../styles/Hero.css";
+import "animate.css";
 
 const Hero = () => {
   return (
     <section className="hero" id="home">
       {/* <img /> */}
-      <div className="bio animate__animated animate__shakeX">
-        <h1 className="bio-title">Hi, my name is Xuan Hien Pham</h1>
+      <div className="bio">
+        <h1 className="bio-title">
+          <span>👋</span>
+          {" Hi, my name is Xuan Hien Pham"}
+        </h1>
         <p className="bio-subtitle">I am a Software Developer</p>
       </div>
     </section>

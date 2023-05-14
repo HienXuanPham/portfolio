a43e1ddfd0a70df7af5750015ced0ed326882a4c
HienXuanPham <phamxuanhien02@gmail.com>
2023-05-13 22:44:25 -0400

    created about me section and added App.css

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,11 +3,11 @@ import "../styles/Hero.css";
 
 const Hero = () => {
   return (
-    <section class="hero" id="home">
+    <section className="hero" id="home">
       {/* <img /> */}
-      <div class="bio animate__animated animate__shakeX">
-        <h1 class="bio-title">Hi, my name is Xuan Hien Pham</h1>
-        <p class="bio-subtitle">I am a Software Developer</p>
+      <div className="bio animate__animated animate__shakeX">
+        <h1 className="bio-title">Hi, my name is Xuan Hien Pham</h1>
+        <p className="bio-subtitle">I am a Software Developer</p>
       </div>
     </section>
   );

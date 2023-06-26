b68ff7792d3834a4c273cc498741575dc7e00dc3
HienXuanPham <phamxuanhien02@gmail.com>
2023-06-25 23:24:21 -0400

    responsive application

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,17 @@
-import React from "react";
+import React, { useRef } from "react";
 import "../styles/NavBar.css";
-import "https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js";
 
 const NavBar = () => {
+  const navRef = useRef();
+
+  const showBurgerNavBar = () => {
+    navRef.current.classList.toggle("show");
+  };
+
+  const hideBurgerNavBar = () => {
+    navRef.current.classList.remove("show");
+  };
+
   return (
     <nav>
       <h1>
@@ -10,30 +19,34 @@ const NavBar = () => {
           Xuan Hien Pham
         </a>
       </h1>
-      <ul className="navigation">
+      <ul className="navigation" ref={navRef}>
         <li>
-          <a href="#about-me" className="nav-link">
+          <a href="#about-me" className="nav-link" onClick={hideBurgerNavBar}>
             About me
           </a>
         </li>
         <li>
-          <a href="#skills" className="nav-link">
+          <a href="#skills" className="nav-link" onClick={hideBurgerNavBar}>
             Skills
           </a>
         </li>
         <li>
-          <a href="#projects" className="nav-link">
+          <a href="#projects" className="nav-link" onClick={hideBurgerNavBar}>
             Projects
           </a>
         </li>
         <li>
-          <a href="#contact" className="nav-link">
+          <a href="#contact" className="nav-link" onClick={hideBurgerNavBar}>
             Contact
           </a>
         </li>
       </ul>
-      <button className="burger-menu" id="burger-menu">
-        <ion-icon className="bars" name="menu-outline"></ion-icon>
+      <button
+        className="burger-menu"
+        id="burger-menu"
+        onClick={showBurgerNavBar}
+      >
+        ☰
       </button>
     </nav>
   );

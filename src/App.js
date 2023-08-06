a15efebe43e061fcd887fc93c6c77eb144b9f94b
HienXuanPham <44250274+HienXuanPham@users.noreply.github.com>
2023-08-05 22:57:11 -0400

    Use formspree to get emails (#9)
    * used formspree to get emails
    
    * added thank you dialog
    
    * if the form hasn't been submitted, renders Contact component. Else, renders ThankYou component.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import React from "react";
+import React, { useState } from "react";
 import "animate.css";
 import NavBar from "./components/NavBar";
 import Hero from "./components/Hero";
@@ -8,8 +8,15 @@ import Skills from "./components/Skills";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 import UpwardArrow from "./components/UpwardArrow";
+import ThankYou from "./components/ThankYou";
 
 function App() {
+  const [contactFormSubmit, setContactFormSubmit] = useState(false);
+
+  const handleContactFormSubmit = () => {
+    setContactFormSubmit(true);
+  };
+
   return (
     <div className="App">
       <NavBar />
@@ -17,7 +24,11 @@ function App() {
       <AboutMe />
       <Skills />
       <Projects />
-      <Contact />
+      {!contactFormSubmit ? (
+        <Contact onFormSubmit={handleContactFormSubmit} />
+      ) : (
+        <ThankYou />
+      )}
       <UpwardArrow />
     </div>
   );
